f85e53dce36aa9be65571e4d3404fa803b1a87d2
Justin Roche <jproche5@gmail.com>
2019-09-24 17:06:56 +0700

    .

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,26 +1,22 @@
+declare var require: any;
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { from } from 'rxjs';
 let m = require('./mock.json')
+
 @Injectable({
     providedIn: 'root'
 })
 export class StorageService {
-
     constructor(private s: Storage) {
         s.set('habits', []);
         // if (m.length) {
         s.set('habits', m);
         // }
     }
-
     load() {
         let p = this.s.get('habits')
+
         return from(p)
     }
-
-
-
-
-
 }
